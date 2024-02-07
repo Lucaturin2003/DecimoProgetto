41944cdcf70c0e2d244fc8b5b71676e21b3e8ab2
Luca Turin <luca@payoff.it>
2024-02-07 09:55:14 +0100

    Commit

---
+++ Delegate/PhotoProcessorHelpers.cs
@@ -0,0 +1,21 @@
+﻿using Delegate;
+
+internal static class PhotoProcessorHelpers
+{
+    public static void Process(Action<Photo> filterHandler, string path)
+    {
+        var photo = Photo.Load(path);
+
+        filterHandler(photo);
+
+        photo.Save();
+    }
+        public static void Process(Action<Photo> filterHandler, string path)
+        {
+            var photo = Photo.Load(path);
+
+            filterHandler(photo);
+
+            photo.Save();
+        }
+}
---
+++ Delegate/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace Delegate
+{
+    class Photo
+    {
+        public string Path;
+        public Photo(string path)
+        {
+            Path = path;
+        }
+
+        public static Photo Load(string path)
+        {
+            return new Photo(path);
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Foto salvata");
+        }
+    }
+
+    public class PhotoFilter
+    {
+        internal void Resize(Photo photo)
+        {
+            Console.WriteLine("Resizing photo");
+        }
+
+        internal void ApplyBrightness(Photo photo)
+        {
+            Console.WriteLine("Apply brightness");
+        }
+
+        internal void ApplyContrast(Photo photo)
+        {
+            Console.WriteLine("Apply contrast");
+        }
+    }
+
+    public class PhotoProcessor
+    {
+        internal void Process(Action<Photo> filterHandler, string path)
+        {
+            var photo = Photo.Load(path);
+
+            filterHandler(photo);
+
+            photo.Save();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var processor = new PhotoProcessor();
+            var filters = new PhotoFilter();
+            Action<Photo> filterHandler = filters.ApplyBrightness;
+            filterHandler += filters.ApplyContrast;
+            filterHandler += RemoveRedEyeFilter;
+            processor.Process(filterHandler, "photo.jpg");
+        }
+
+        static void RemoveRedEyeFilter(Photo photo)
+        {
+            Console.WriteLine("Apply remove red eye");
+        }
+    }
+}
